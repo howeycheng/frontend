ea6b80da4fac35d2646084b17232d7b6d01976b7
howeycheng@163.com <3844321319git>
2019-09-04 00:27:58 +0800

    增加场景组件、场景用例名称初步显示

--- src/components/Cases.vue
+++ src/components/Cases.vue
@@ -1,13 +1,37 @@
 <template>
     <div>
         <headers></headers>
-        <div>
-            {{id}}
+        <div style="display: inline">
+            <div style="display: inline">
+                <el-table
+                        :data="scenesSet"
+                        border="True"
+                        style="align-content: center"
+                        highlight-current-row>
+                    <el-table-column
+                            prop=case_name
+                            label="组件">
+                    </el-table-column>
+                </el-table>
+            </div>
+            <div style="display: inline">
+                <el-table
+                        :data="scenesCases"
+                        border="True"
+                        style="align-content: center"
+                        highlight-current-row>
+                    <el-table-column
+                            prop=name
+                            label="用例名称">
+                    </el-table-column>
+                </el-table>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     import headers from './header'
 
     export default {
@@ -15,10 +39,41 @@
         components: {
             headers
         },
-        data(){
-            return{
-                id:this.$route.query.rqid
+        data() {
+            return {
+                id: this.$route.query.rqid,
+                scenesSet: [],
+                scenesCases: []
+
             }
+        },
+        mounted: function () {
+            this.getScenesSet(this.id);
+            this.getScenesCases(this.id);
+        },
+        methods: {
+            getScenesSet(id) {
+                axios.get("http://127.0.0.1:8000/atf/sceneDetail/", {
+                    params: {
+                        rqid: id
+                    }
+                }).then(
+                    response => {
+                        this.scenesSet = response.data
+                    }
+                )
+            },
+            getScenesCases(id) {
+                axios.get("http://127.0.0.1:8000/atf/cases/", {
+                    params: {
+                        rqid: id
+                    }
+                }).then(
+                    response => {
+                        this.scenesCases = response.data
+                    }
+                )
+            },
         }
 
     }
