a913513314b54e23d11ccaeec714bc9515d49472
howeycheng@163.com <3844321319git>
2019-09-04 23:48:17 +0800

    增加组件配置和用例数据标签分页 增加值传递和校验点标签页设计

--- src/components/Cases.vue
+++ src/components/Cases.vue
@@ -1,31 +1,56 @@
 <template>
     <div>
         <headers></headers>
-        <div style="display: inline">
-            <div style="display: inline">
-                <el-table
-                        :data="scenesSet"
-                        border="True"
-                        style="align-content: center"
-                        highlight-current-row>
-                    <el-table-column
-                            prop=case_name
-                            label="组件">
-                    </el-table-column>
-                </el-table>
-            </div>
-            <div style="display: inline">
-                <el-table
-                        :data="scenesCases"
-                        border="True"
-                        style="align-content: center"
-                        highlight-current-row>
-                    <el-table-column
-                            prop=name
-                            label="用例名称">
-                    </el-table-column>
-                </el-table>
-            </div>
+        <div id="detail">
+            <el-tabs id="tabs" v-model="activeName" @tab-click="handleClick">
+                <el-tab-pane label="组件配置" name="first">
+                    <el-table
+                            :data="scenesSet"
+                            border="True"
+                            style="align-content: center"
+                            highlight-current-row>
+                        <el-table-column
+                                prop=case_name
+                                label="组件">
+                        </el-table-column>
+                    </el-table>
+                    <div class="set_params">
+                        <el-tabs v-model="activeName2" @tab-click="handleClick">
+                            <el-tab-pane label="数值传递" name="first">
+                                用户管理
+                            </el-tab-pane>
+                            <el-tab-pane label="数值校验" name="second">
+                                配置管理
+                            </el-tab-pane>
+                        </el-tabs>
+                    </div>
+                </el-tab-pane>
+                <el-tab-pane label="用例数据" name="second">
+                    <div class="block">
+                        <div class="block">
+                            <el-pagination
+                                    @size-change="handleSizeChange"
+                                    @current-change="handleCurrentChange"
+                                    :current-page="currentPage4"
+                                    :page-sizes="[100, 200, 300, 400]"
+                                    :page-size="100"
+                                    layout="total, sizes, prev, pager, next, jumper"
+                                    :total="400">
+                            </el-pagination>
+                        </div>
+                    </div>
+                    <el-table
+                            :data="scenesCases"
+                            border="True"
+                            style="align-content: center"
+                            highlight-current-row>
+                        <el-table-column
+                                prop=name
+                                label="用例名称">
+                        </el-table-column>
+                    </el-table>
+                </el-tab-pane>
+            </el-tabs>
         </div>
     </div>
 </template>
@@ -43,7 +68,9 @@
             return {
                 id: this.$route.query.rqid,
                 scenesSet: [],
-                scenesCases: []
+                scenesCases: [],
+                activeName: 'first',
+                activeName2: 'first'
 
             }
         },
@@ -80,5 +107,8 @@
 </script>
 
 <style scoped>
+    #detail {
+        padding-left: 30px;
+    }
 
 </style>
